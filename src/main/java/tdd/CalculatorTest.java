a43e220e105e87a5850bcf1631ea37d79994c790
ttasjwi <ttasjwi920@gmail.com>
2022-07-11 15:40:30 +0900

    [ttasjwi#1] Test: 메서드 별 테스트 메서드 분리
    - `main()` 메서드 하나로 모두 테스트하기 때문에, 모든 메서드를 테스트할 수 밖에 없음
    - 콘솔에 출력되는 값을 통해 테스트 결과를 매번 확인해야한다.
      - 로직이 복잡해지면, 매 테스트 코드마다 복잡한 로직을 매번 고려하여 정상적으로 출력되는지 일일이 확인해야함.

--- src/main/java/tdd/CalculatorTest.java
+++ src/main/java/tdd/CalculatorTest.java
@@ -1,12 +1,33 @@
-    package tdd;
+package tdd;
 
-    public class CalculatorTest {
+public class CalculatorTest {
 
-        public static void main(String[] args) {
-            Calculator cal = new Calculator();
-            System.out.println(cal.add(9,3));
-            System.out.println(cal.subtract(9,3));
-            System.out.println(cal.multiply(9,3));
-            System.out.println(cal.divide(9,3));
-        }
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        addTest(cal);
+        subtractTest(cal);
+        multiplyTest(cal);
+        divideTest(cal);
     }
+
+    private static void addTest(Calculator cal) {
+        System.out.println("Calculator의 add 메서드 인자로 9,3이 주어지면 12를 반환해야한다.");
+        System.out.println(cal.add(9, 3));
+    }
+
+    private static void subtractTest(Calculator cal) {
+        System.out.println("Calculator의 subtract 메서드 인자로 9,3이 주어지면 6을 반환해야한다.");
+        System.out.println(cal.subtract(9, 3));
+    }
+
+    private static void multiplyTest(Calculator cal) {
+        System.out.println("Calculator의 multiply 메서드 인자로 9,3이 주어지면 27을 반환해야한다.");
+        System.out.println(cal.multiply(9, 3));
+    }
+
+    private static void divideTest(Calculator cal) {
+        System.out.println("Calculator의 divide 메서드 인자로 9,3이 주어지면 3을 반환해야한다.");
+        System.out.println(cal.divide(9, 3));
+    }
+
+}
