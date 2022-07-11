f01af47a1684fb7b7c3215f16c98c75db8807c8c
ttasjwi <ttasjwi920@gmail.com>
2022-07-11 16:31:17 +0900

    [ttasjwi#1] Test: 결과 값을 눈이 아닌 프로그램을 통해 자동화
    - JUnit이 지원하는 테스트 메서드를 활용
      - Assertions.assertEquals(기댓값, 메서드 결과 값)
      - cf) import static 문으로 Assertions를 호출하는 것을 간소화
    - 눈으로 테스트하지 않고, 결과가 맞으면 통과. 결과가 다르면 통과되지 않음
    - assertEquals, assertTrue, assertFalse, assertNull, assertNotNull, assertArrayEquals 등 제공

--- src/test/java/tdd/CalculatorTest.java
+++ src/test/java/tdd/CalculatorTest.java
@@ -3,6 +3,8 @@ package tdd;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @DisplayName("Calculator의")
 class CalculatorTest {
 
@@ -10,27 +12,27 @@ class CalculatorTest {
     @DisplayName("add 메서드 인자로 9,3이 주어지면 12를 반환해야한다.")
     void addTest() {
         Calculator cal = new Calculator();
-        System.out.println(cal.add(9, 3));
+        assertEquals(12, cal.add(9,3));
     }
 
     @Test
     @DisplayName("subtract 메서드 인자로 9,3이 주어지면 6을 반환해야한다.")
     void subtractTest() {
         Calculator cal = new Calculator();
-        System.out.println(cal.subtract(9, 3));
+        assertEquals(6, cal.subtract(9,3));
     }
 
     @Test
     @DisplayName("multiply 메서드 인자로 9,3이 주어지면 27을 반환해야한다.")
     void multiplyTest() {
         Calculator cal = new Calculator();
-        System.out.println(cal.multiply(9, 3));
+        assertEquals(27, cal.multiply(9,3));
     }
 
     @Test
     @DisplayName("divide 메서드 인자로 9,3이 주어지면 3을 반환해야한다.")
     void divideTest() {
         Calculator cal = new Calculator();
-        System.out.println(cal.divide(9, 3));
+        assertEquals(3, cal.divide(9,3));
     }
 }
