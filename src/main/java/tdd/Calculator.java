390069dd23140a4860b547d28ca50e39a8777cf2
ttasjwi <ttasjwi920@gmail.com>
2022-07-11 15:17:27 +0900

    [ttasjwi#1] Test: Calculator 프로덕션 코드, 테스트 코드 분리
    - `main()` 메서드에서 여러 메서드를 동시에 테스트하고 있음.
    - 프로덕션 코드의 복잡도가 증가할 수록 `main()` 메서드의 복잡도 증가 -> `main()`메서드의 유지 부담 증가

--- src/main/java/tdd/Calculator.java
+++ src/main/java/tdd/Calculator.java
@@ -30,11 +30,4 @@ public class Calculator {
         return i / j;
     }
 
-    public static void main(String[] args) {
-        Calculator cal = new Calculator();
-        System.out.println(cal.add(9,3));
-        System.out.println(cal.subtract(9,3));
-        System.out.println(cal.multiply(9,3));
-        System.out.println(cal.divide(9,3));
-    }
 }
---
+++ src/main/java/tdd/CalculatorTest.java
@@ -0,0 +1,12 @@
+    package tdd;
+
+    public class CalculatorTest {
+
+        public static void main(String[] args) {
+            Calculator cal = new Calculator();
+            System.out.println(cal.add(9,3));
+            System.out.println(cal.subtract(9,3));
+            System.out.println(cal.multiply(9,3));
+            System.out.println(cal.divide(9,3));
+        }
+    }
