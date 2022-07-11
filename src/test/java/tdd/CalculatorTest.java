e1974e8bd24a6606767f74173f989966ec32bf2d
ttasjwi <ttasjwi920@gmail.com>
2022-07-11 17:00:54 +0900

    [ttasjwi#1] Test: AssertJ를 활용하여 테스트 코드 가독성 향상
    - JUnit은 테스트 코드의 가독성이 좋지 못 함
      - Junit : Assertions.assertEquals(기댓값, 메서드 결과 값)
    - AssertJ를 사용하여 Assertions의 불편한 가독성을 개선
      - AssertJ : Assertions.assertThat(메서드 결과값).isEqualsTo(기댓값)
      - AssertJ도 import static 문을 지원
    - 국내 많은 개발자들은 순수 JUnit을 사용한 테스트보다 AssertJ를 사용하여 테스트하는 것을 선호

--- src/test/java/tdd/CalculatorTest.java
+++ src/test/java/tdd/CalculatorTest.java
@@ -1,8 +1,11 @@
 package tdd;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @DisplayName("Calculator의")
 class CalculatorTest {
@@ -17,25 +20,25 @@ class CalculatorTest {
     @Test
     @DisplayName("add 메서드 인자로 9,3이 주어지면 12를 반환해야한다.")
     void addTest() {
-        assertEquals(12, cal.add(9,3));
+        assertThat(cal.add(9,3)).isEqualTo(12);
     }
 
     @Test
     @DisplayName("subtract 메서드 인자로 9,3이 주어지면 6을 반환해야한다.")
     void subtractTest() {
-        assertEquals(6, cal.subtract(9,3));
+        assertThat(cal.subtract(9,3)).isEqualTo(6);
     }
 
     @Test
     @DisplayName("multiply 메서드 인자로 9,3이 주어지면 27을 반환해야한다.")
     void multiplyTest() {
-        assertEquals(27, cal.multiply(9,3));
+        assertThat(cal.multiply(9,3)).isEqualTo(27);
     }
 
     @Test
     @DisplayName("divide 메서드 인자로 9,3이 주어지면 3을 반환해야한다.")
     void divideTest() {
-        assertEquals(3, cal.divide(9,3));
+        assertThat(cal.divide(9,3)).isEqualTo(3);
     }
 
 }
