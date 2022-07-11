d87f00f37012586069007972e1b1aed25331dbe7
ttasjwi <ttasjwi920@gmail.com>
2022-07-11 16:49:02 +0900

    [ttasjwi#1] Test: 테스트 코드 중복 코드 제거 - beforeEach 활용
    - 각 메서드에서 공통적으로 사용하는 인스턴스를 필드에서 관리하여 중복 제거
    - 각 테스트별로 독립적으로 초기화된 인스턴스를 사용하기 위해 beforeEach 메서드를 통해 매번 필드 초기화
      - 필드에서 초기화하면 매 테스트마다 인스턴스의 상태가 변경되고, 다음 테스트에 영향을 미칠 수 있기 때문
    - JUnit은 beforeEach, beforeAll, afterEach, afterAll 메서드를 지원
      - beforeAll : 모든 테스트가 실행되기 전(제일 처음, static)
      - beforeEach : 각 테스트가 실행되기 직전
      - afterEach : 각 테스트가 실행된 직후
      - afterAll : 모든 테스트가 실행된 후(제일 마지막, static)

--- src/test/java/tdd/CalculatorTest.java
+++ src/test/java/tdd/CalculatorTest.java
@@ -1,38 +1,41 @@
 package tdd;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Calculator의")
 class CalculatorTest {
 
+    private Calculator cal;
+
+    @BeforeEach
+    public void setUp() {
+        cal = new Calculator();
+    }
+
     @Test
     @DisplayName("add 메서드 인자로 9,3이 주어지면 12를 반환해야한다.")
     void addTest() {
-        Calculator cal = new Calculator();
         assertEquals(12, cal.add(9,3));
     }
 
     @Test
     @DisplayName("subtract 메서드 인자로 9,3이 주어지면 6을 반환해야한다.")
     void subtractTest() {
-        Calculator cal = new Calculator();
         assertEquals(6, cal.subtract(9,3));
     }
 
     @Test
     @DisplayName("multiply 메서드 인자로 9,3이 주어지면 27을 반환해야한다.")
     void multiplyTest() {
-        Calculator cal = new Calculator();
         assertEquals(27, cal.multiply(9,3));
     }
 
     @Test
     @DisplayName("divide 메서드 인자로 9,3이 주어지면 3을 반환해야한다.")
     void divideTest() {
-        Calculator cal = new Calculator();
         assertEquals(3, cal.divide(9,3));
     }
+
 }
