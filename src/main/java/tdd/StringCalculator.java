d57edc5b2b95d9258e84eb16d150a6397238983e
ttasjwi <ttasjwi920@gmail.com>
2022-07-12 11:26:01 +0900

    [ttasjwi#3] Feat: StringCalculator.parseNonNegativeIntegers 추가
    - 문자열 리스트가 주어졌을 때 이들을 숫자로 파싱

--- src/main/java/tdd/StringCalculator.java
+++ src/main/java/tdd/StringCalculator.java
@@ -1,11 +1,32 @@
 package tdd;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class StringCalculator {
 
     private static final String DEFAULT_WORD_SPLIT_DELIMITER = "[,:]";
+
+    /**
+     * 문자열 리스트가 주어질 때 각각을 음이 아닌 정수로 파싱
+     *  1. 빈 문자열 요소, 공백문자열 요소, null 요소는 0으로 파싱
+     *  2. null -> 빈 리스트 반환
+     *  3. 빈 리스트 -> 빈 리스트 반환
+     *  4. 하나라도 형식에 안 맞으면 NumberFormatException 발생
+     *  5. 하나라도 음수면 RuntimeException 발생
+     */
+    public List<Integer> parseNonNegativeIntegers(List<String> strs) {
+        if (Objects.isNull(strs)) {
+            return new ArrayList<>();
+        }
+
+        return strs.stream()
+                .map(this::parseNonNegativeInteger)
+                .collect(Collectors.toList());
+    }
 
     /**
      * 문자열이 주어지면 구분자로 분리하고 리스트로 반환.
