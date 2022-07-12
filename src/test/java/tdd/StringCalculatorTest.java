e9d2de199458ea2f1d86a41c4fb4e13dc8ff0bf3
ttasjwi <ttasjwi920@gmail.com>
2022-07-12 11:26:35 +0900

    [ttasjwi#3] Test: StringCalculator.parseNonNegativeIntegers 테스트 추가

--- src/test/java/tdd/StringCalculatorTest.java
+++ src/test/java/tdd/StringCalculatorTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -160,5 +161,104 @@ class StringCalculatorTest {
             // then
             assertThat(result).containsExactlyInAnyOrder("가", "나", "다");
         }
+    }
+
+    @Nested
+    @DisplayName("parseNonNegativeIntegers 메서드는")
+    class test_parseNonNegativeIntegers {
+
+        @Test
+        @DisplayName("리스트가 null -> 빈 리스트 반환")
+        public void nullListTest() {
+            //given
+            List<String> strs = null;
+
+            //when
+            List<Integer> numbers = strCal.parseNonNegativeIntegers(strs);
+
+            //then
+            assertThat(numbers).isEmpty();
+        }
+
+        @Test
+        @DisplayName("빈 리스트 -> 빈 리스트 반환")
+        public void emptyListTest() {
+            //given
+            List<String> strs = new ArrayList<>();
+
+            //when
+            List<Integer> numbers = strCal.parseNonNegativeIntegers(strs);
+
+            //then
+            assertThat(numbers).isEmpty();
+        }
+
+        @Test
+        @DisplayName("하나라도 형식에 맞지 않는 문자열 있음 -> NumberFormatException 발생")
+        public void strangeWordContains_Test() {
+            //given
+            List<String> strs = List.of("가", "1", "2");
+
+            //when & then
+            assertThatThrownBy(() -> strCal.parseNonNegativeIntegers(strs))
+                    .isInstanceOf(NumberFormatException.class);
+        }
+
+
+        @Test
+        @DisplayName("하나라도 음수가 있음 -> RuntimeException 발생")
+        public void negativeIntegerContains_Test() {
+            //given
+            List<String> strs = List.of("-1", "1", "2");
+
+            //when & then
+            assertThatThrownBy(() -> strCal.parseNonNegativeIntegers(strs))
+                    .isInstanceOf(RuntimeException.class);
+        }
+
+        @Test
+        @DisplayName("0 또는 양의 정수 -> 같은 값 리스트 반환")
+        public void nonNegativeNumbersTest() {
+            // given
+            List<String> strs = List.of("0", "1", "2", "3", "5", "7", "65535", "2147483647");
+
+            // when
+            List<Integer> numbers = strCal.parseNonNegativeIntegers(strs);
+
+            // then
+            assertThat(numbers)
+                    .containsExactlyInAnyOrder(0, 1, 2, 3, 5, 7, 65535, 2147483647);
+        }
+
+        @Test
+        @DisplayName("공백 -> 0 반환")
+        public void emptyStrContainsTest() {
+            // given
+            List<String> strs = List.of("", "1", "2", "5");
+
+            // when
+            List<Integer> numbers = strCal.parseNonNegativeIntegers(strs);
+
+            // then
+            assertThat(numbers)
+                    .containsExactlyInAnyOrder(0, 1, 2, 5);
+        }
+
+        @Test
+        @DisplayName("null 요소 -> 0 반환")
+        public void nullContainsTest() {
+            // given
+            List<String> strs = new ArrayList<>();
+            strs.add(null);
+            strs.add("1");
+
+            // when
+            List<Integer> numbers = strCal.parseNonNegativeIntegers(strs);
+
+            // then
+            assertThat(numbers)
+                    .containsExactlyInAnyOrder(0, 1);
+        }
+
     }
 }
