541dd34a992d689bd25426feca04069e387e9087
ttasjwi <ttasjwi920@gmail.com>
2022-07-20 19:01:04 +0900

    [ttasjwi#6] Feat: GET 요청으로 회원 가입을 할 수 있도록 함

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -8,7 +8,10 @@ import java.util.Objects;
 
 import exception.HttpRequestMessageException;
 import exception.NullRequestLineException;
+import http.request.HttpMethod;
 import http.request.RequestLine;
+import member.dto.MemberJoinRequestDto;
+import member.service.MemberService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,12 +19,15 @@ public class RequestHandler extends Thread {
 
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    private static final String REQUEST_LINE_DELIMITER = " +";
-
     private Socket connection;
+
+    private final MemberService memberService;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
+
+        AppConfig appConfig = AppConfig.getSingletonInstance();
+        memberService = appConfig.memberService();
     }
 
     public void run() {
@@ -33,26 +39,45 @@ public class RequestHandler extends Thread {
 
             // RequestLine 읽기
             RequestLine requestLine = readRequestLine(br);
-            String url = requestLine.getUrl();
             log.debug("Request Line : {}", requestLine);
 
-            String requestHeader;
-
             // 요청 헤더들을 마지막까지 읽기
+            String requestHeader;
             while (!(requestHeader = br.readLine()).isEmpty()) {
                 log.debug("Request Header/ {}", requestHeader);
             }
-
-            // 요청을 분석하고 응답하기
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp" + url). toPath());
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            sendResponse(requestLine, out);
         } catch (IOException e) {
             log.error(e.getMessage());
         } catch (HttpRequestMessageException me) {
             log.error(me.getMessage());
         }
+    }
+
+    private void sendResponse(RequestLine requestLine, OutputStream out) throws IOException {
+        // 요청을 분석하고 응답하기
+        String url = requestLine.getUrl();
+        HttpMethod method = requestLine.getMethod();
+
+        if (url.equals("/members/create") && method == HttpMethod.GET) {
+            log.info("회원 가입 요청");
+            joinMember(requestLine);
+        }
+        DataOutputStream dos = new DataOutputStream(out);
+        byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        response200Header(dos, body.length);
+        responseBody(dos, body);
+    }
+
+    private void joinMember(RequestLine requestLine) {
+        MemberJoinRequestDto memberJoinRequestDto = MemberJoinRequestDto.builder()
+                .loginId(requestLine.getParameter("loginId"))
+                .password(requestLine.getParameter("password"))
+                .name(requestLine.getParameter("name"))
+                .email(requestLine.getParameter("email"))
+                .build();
+
+        memberService.join(memberJoinRequestDto);
     }
 
     private RequestLine readRequestLine(BufferedReader br) throws IOException {
