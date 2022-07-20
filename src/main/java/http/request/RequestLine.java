7d5c90763f6f2bb9b4ca0c93d87bd25c2cf9e161
ttasjwi <ttasjwi920@gmail.com>
2022-07-20 15:58:17 +0900

    [ttasjwi#4] Feat : RequestLine에 쿼리 파라미터 분리 기능 추가

--- src/main/java/http/request/RequestLine.java
+++ src/main/java/http/request/RequestLine.java
@@ -3,34 +3,56 @@ package http.request;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+import util.HttpRequestUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 @ToString(of = {"method", "url", "protocol"})
 public class RequestLine {
 
     private static final String REQUEST_LINE_DELIMITER = " +";
-    private String method;
-    private String url;
-    private String protocol;
+    private static final String QUESTION_MARK = "?";
+
+    private final String method;
+    private final String url;
+    private final String protocol;
+    private final Map<String, String> queryParameters = new HashMap<>();
 
     @Builder
-    public RequestLine(String method, String url, String protocol) {
+    public RequestLine(String method, String url, String protocol, Map<String, String> queryParameters) {
         this.method = method;
         this.url = url;
         this.protocol = protocol;
+        this.queryParameters.putAll(queryParameters);
     }
 
     /**
      * Http 요청의 첫번째 라인을 기반으로 RequestLine 객체 생성
      */
     public static RequestLine of(String requestLineString) {
-        String[] token = requestLineString.split(REQUEST_LINE_DELIMITER);
+        String decodedString = HttpRequestUtils.decodeURLEncoding(requestLineString);
+        
+        String[] requestLineTokens = decodedString.split(REQUEST_LINE_DELIMITER);
+        String method = requestLineTokens[0];
+        String url = requestLineTokens[1];
+        String queryString = "";
+        String protocol = requestLineTokens[2];
+        
+        if (url.contains(QUESTION_MARK)) {
+            int indexOfQuestionMark = url.indexOf(QUESTION_MARK);
+            queryString = url.substring(indexOfQuestionMark + 1);
+            url = url.substring(0, indexOfQuestionMark);
+        }
 
-        return RequestLine.builder()
-                .method(token[0])
-                .url(token[1])
-                .protocol(token[2])
-                .build();
+        Map<String, String> queryParameters = HttpRequestUtils.parseQueryString(queryString);
+
+        return new RequestLine(method, url, protocol, queryParameters);
+    }
+
+    public String getParameter(String key) {
+        return queryParameters.get(key);
     }
 
 }
