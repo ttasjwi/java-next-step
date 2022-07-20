64f43ee67cbce048f6df4802cb58fdc3104be3b8
ttasjwi <ttasjwi920@gmail.com>
2022-07-20 11:11:46 +0900

    [ttasjwi#4] Refactor: 서버 실행 시 서버포트를 지정하는 메서드 분리

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -11,15 +11,9 @@ public class WebServer {
     private static final int DEFAULT_PORT = 8080;
 
     public static void main(String args[]) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
-        }
+        int port = initPort(args);
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
-
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             log.info("Web Application Server started {} port.", port);
 
@@ -30,5 +24,26 @@ public class WebServer {
                 requestHandler.start();
             }
         }
+    }
+
+    /**
+     *
+     자바 실행 과정에서 명령으로 넘겨진 인자가 있는지 확인 후 서버 포트를 확인.
+
+     1. 포트를 지정하면 지정 포트를 사용
+     2. 지정하지 않았을 경우 기본 포트인 8080 포트를 사용한다.
+
+     */
+    private static int initPort(String[] args) {
+        return (isEmptyArguments(args))
+                ? DEFAULT_PORT
+                : Integer.parseInt(args[0]);
+    }
+
+    /**
+     * 자바 실행 시, args가 존재하는 지 확인
+     */
+    private static boolean isEmptyArguments(String[] args) {
+        return args == null || args.length == 0;
     }
 }
