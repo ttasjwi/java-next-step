a2eccecb68cade7055442065c0c9b80738ced38b
ttasjwi <ttasjwi920@gmail.com>
2022-07-20 16:50:35 +0900

    [ttasjwi#6] Refactor: User->Member, DataBase -> MemberRepository 변경

---
+++ src/main/java/db/MemberRepository.java
@@ -0,0 +1,51 @@
+package db;
+
+import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
+import model.Member;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Slf4j
+public class MemberRepository {
+
+    private final Map<Long, Member> store = Maps.newHashMap();
+    private final AtomicLong sequence = new AtomicLong();
+
+    /**
+     * 회원 저장
+     */
+    public Long save(Member member) {
+        Long id = sequence.incrementAndGet();
+        member.initId(id);
+        store.put(member.getId(), member);
+        return id;
+    }
+
+    /**
+     * 식별자로 회원 조회
+     */
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    /**
+     * 로그인아이디로 조회
+     */
+    public Optional<Member> findByLoginId(String loginId) {
+        return store.values().stream()
+                .filter(member -> member.getLoginId().equals(loginId))
+                .findFirst();
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
---
+++ src/main/java/model/Member.java
@@ -0,0 +1,28 @@
+package model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString(of = {"id", "loginId", "password", "name", "email"})
+public class Member {
+
+    private Long id;
+    private String loginId;
+    private String password;
+    private String name;
+    private String email;
+
+    @Builder
+    public Member(String loginId, String password, String name, String email) {
+        this.loginId = loginId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public void initId(Long id) {
+        this.id = id;
+    }
+}
