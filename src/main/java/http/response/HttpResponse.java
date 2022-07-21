22922f36a18360648853c658c7712cfc831dfe31
ttasjwi <ttasjwi920@gmail.com>
2022-07-21 14:23:34 +0900

    [ttasjwi#9] Feat: HttpResponse - 쿠키 추가, 응답 기능 추가

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -1,14 +1,20 @@
 package http.response;
 
+import http.common.Cookie;
+
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HttpResponse {
 
     private final DataOutputStream dos;
+    private final List<Cookie> cookies = new ArrayList<>();
+
     public HttpResponse(OutputStream out) throws IOException {
         this.dos = new DataOutputStream(out);
     }
@@ -21,6 +27,7 @@ public class HttpResponse {
         dos.writeBytes("HTTP/1.1 200 OK \r\n");
         dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
         dos.writeBytes("Content-Length: " + body.length + "\r\n");
+        responseCookies();
         dos.writeBytes("\r\n");
         responseBody(body);
     }
@@ -36,6 +43,23 @@ public class HttpResponse {
     public void redirectTo(String redirectUrl) throws IOException {
         dos.writeBytes("HTTP/1.1 302 Found \r\n");
         dos.writeBytes("Location: " + redirectUrl + "\r\n");
+        responseCookies();
         dos.writeBytes("\r\n");
+    }
+
+    public void addCookie(Cookie cookie) {
+        cookies.add(cookie);
+    }
+
+    private void responseCookies() throws IOException {
+        for (Cookie cookie : cookies) {
+            dos.writeBytes(String.format("Set-Cookie: %s=%s;", cookie.getKey(),cookie.getValue()));
+
+            if (cookie.getMaxAge() != null) {
+                dos.writeBytes("max-age="+cookie.getMaxAge()+";");
+            }
+
+            dos.writeBytes("path=/\r\n");
+        }
     }
 }
