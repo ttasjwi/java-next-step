63bedbdce53c842c8166432ea189414a62f70f88
ttasjwi <ttasjwi920@gmail.com>
2022-07-21 13:19:20 +0900

    [ttasjwi#8] Refactor: RequestHandler에서, 기존 응답 로직을 HttpResponse에게 위임

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,14 +3,16 @@ package webserver;
 import exception.HttpRequestMessageException;
 import http.request.HttpMethod;
 import http.request.HttpRequest;
+import http.response.HttpResponse;
 import member.dto.MemberJoinRequestDto;
 import member.service.MemberService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
 
@@ -33,7 +35,8 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             HttpRequest request = HttpRequest.of(in);
-            sendResponse(request, out);
+            HttpResponse response = new HttpResponse(out);
+            sendResponse(request, response);
         } catch (IOException e) {
             log.error(e.getMessage());
         } catch (HttpRequestMessageException me) {
@@ -41,7 +44,7 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private void sendResponse(HttpRequest request, OutputStream out) throws IOException {
+    private void sendResponse(HttpRequest request, HttpResponse response) throws IOException {
         // 요청을 분석하고 응답하기
         String url = request.getUrl();
         HttpMethod method = request.getMethod();
@@ -50,10 +53,7 @@ public class RequestHandler extends Thread {
             log.info("회원 가입 요청");
             joinMember(request);
         }
-        DataOutputStream dos = new DataOutputStream(out);
-        byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-        response200Header(dos, body.length);
-        responseBody(dos, body);
+        response.responseStaticResource(url);
     }
 
     private void joinMember(HttpRequest request) {
@@ -67,23 +67,4 @@ public class RequestHandler extends Thread {
         memberService.join(memberJoinRequestDto);
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
 }
